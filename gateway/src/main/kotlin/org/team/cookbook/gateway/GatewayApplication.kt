cc6c0c19897ddbd54977bc764730282342c54183
Ilya Zeliankevich <v-ilya.z@turvo.com>
2019-03-04 19:50:44 +0300

    add infrastructure services

--- gateway/src/main/kotlin/org/team/cookbook/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/org/team/cookbook/gateway/GatewayApplication.kt
@@ -1,13 +1,15 @@
-package org.team.cookbook.ingredient
+package org.team.cookbook.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-class IngredientApplication
+class GatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<IngredientApplication>(*args)
+	runApplication<GatewayApplication>(*args)
 }

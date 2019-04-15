11e6a5b3572a9e73cbadfa89d2919d5e9f15ffd8
zombitrafik <zombitrafik05@gmail.com>
2019-04-15 08:41:46 +0300

    fix api

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,45 +19,45 @@ export default {
     },
     actions: {
         async GET_DISHES(context: any): Promise<any> {
-            const {data: dishes} = await Axios.get('http://82.209.201.172:9003/dishes');
+            const {data: dishes} = await Axios.get('/api/dishes');
             context.commit('SET_DISHES', dishes);
         },
         async GET_INGREDIENTS(context: any): Promise<any> {
-            const {data: ingredients} = await Axios.get('http://82.209.201.172:9003/ingredients');
+            const {data: ingredients} = await Axios.get('/api/ingredients');
             context.commit('SET_INGREDIENTS', ingredients);
         },
         async CREATE_DISH(context: any, payload: any): Promise<any> {
-            await Axios.post('http://82.209.201.172:9003/dishes', payload);
+            await Axios.post('/api/dishes', payload);
         },
         async UPDATE_DISH(context: any, payload: any): Promise<any> {
-            await Axios.put(`http://82.209.201.172:9003/dishes/${payload.id}`, payload);
+            await Axios.put(`/api/dishes/${payload.id}`, payload);
         },
         async GET_DISH_BY_ID(context: any, id: any): Promise<any> {
-            const {data: dish} = await Axios.get(`http://82.209.201.172:9003/dishes/${id}`);
+            const {data: dish} = await Axios.get(`/api/dishes/${id}`);
             return dish;
         },
         async CREATE_INGREDIENT(context: any, payload: any): Promise<any> {
-            await Axios.post('http://82.209.201.172:9003/ingredients', payload);
+            await Axios.post('/api/ingredients', payload);
         },
         async UPDATE_INGREDIENT(context: any, payload: any): Promise<any> {
-            await Axios.put(`http://82.209.201.172:9003/ingredients/${payload.id}`, payload);
+            await Axios.put(`/api/ingredients/${payload.id}`, payload);
         },
         async GET_INGREDIENT_BY_ID(context: any, id: any): Promise<any> {
-            const {data: ingredient} = await Axios.get(`http://82.209.201.172:9003/ingredients/${id}`);
+            const {data: ingredient} = await Axios.get(`/api/ingredients/${id}`);
             return ingredient;
         },
         async GET_PRODUCTS(context: any): Promise<any> {
-            const {data: products} = await Axios.get('http://82.209.201.172:9003/products');
+            const {data: products} = await Axios.get('/api/products');
             context.commit('SET_PRODUCTS', products);
         },
         async CREATE_PRODUCT(context: any, payload: any): Promise<any> {
-            await Axios.post('http://82.209.201.172:9003/products', payload);
+            await Axios.post('/api/products', payload);
         },
         async UPDATE_PRODUCT(context: any, payload: any): Promise<any> {
-            await Axios.put(`http://82.209.201.172:9003/products/${payload.id}`, payload);
+            await Axios.put(`/api/products/${payload.id}`, payload);
         },
         async GET_PRODUCT_BY_ID(context: any, id: any): Promise<any> {
-            const {data: product} = await Axios.get(`http://82.209.201.172:9003/products/${id}`);
+            const {data: product} = await Axios.get(`/api/products/${id}`);
             return product;
         },
     }
